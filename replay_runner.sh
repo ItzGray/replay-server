342cc98debf1df926e7f0e33aa8fd2e6573a51e0
ItzGray <63925441+ItzGray@users.noreply.github.com>
2026-09-03 12:39:15 -0500

    Hopefully replay runner works now

--- replay_runner.sh
+++ replay_runner.sh
@@ -1,6 +1,8 @@
 trap "echo Exited!; exit;" SIGINT SIGTERM
 while [[ 1=1 ]]
 do
-	watch --chgexit -n 5 "ls -Ral --full-time ../pokemon-showdown/logs/ | sha256sum" && python3 generate_replays.py && python3 generate_csv.py
+	watch --chgexit -n 5 "ls -Ral --full-time ../pokemon-showdown/logs/ | sha256sum"
+	python3 generate_replays.py
+	python3 generate_csv.py
 	sleep 5
 done
